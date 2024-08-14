e18253e1f5ba3f3a7d7454009192e543d24b4d36
Allison <allisonabinger@gmail.com>
2024-08-13 20:34:48 -0500

    A - 08/13 08:34 PM - Working on styles, font and logo are imported and spaced, but colors and elements need help.

--- client/hero-of-flavor/src/pages/Home.js
+++ client/hero-of-flavor/src/pages/Home.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
+import logoTextImage from '../images/assets/LogoText.png'
 
 const Home = () => {
   return (
-    <div style={styles.container}>
-      <h1>Recipes by Ingredient Tool</h1>
+    <div style={styles.container} className='bannerContainer'>
+      <img alt='Logo with Title' src={logoTextImage} id='logoText'></img>
       <Link to="/recipes">
-        <button style={styles.button}>View all Recipes Here</button>
+        <button style={styles.button}>View All Recipes Here</button>
       </Link>
       <h2>Featured Recipes</h2>
       <div style={styles.featuredRecipes}>
@@ -23,13 +24,19 @@ const styles = {
   container: {
     padding: '20px',
     textAlign: 'center',
-    backgroundColor: '#DFF5E1',
+    backgroundColor: '#394043',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
   },
   button: {
     padding: '10px 20px',
     margin: '20px 0',
-    fontSize: '16px',
+    fontSize: '25px',
     cursor: 'pointer',
+    borderRadius: '15px',
+    fontFamily: 'HyliaSerif',
+    letterSpacing: '1px'
   },
   featuredRecipes: {
     display: 'flex',
